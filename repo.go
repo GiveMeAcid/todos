f9aa47ab511d9d62cd806ee25beea38eb9cc2b41
GiveMeAcid <mazutichek@gmail.com>
2016-12-07 13:00:07 +0200

    added new func to init

--- repo.go
+++ repo.go
@@ -1,50 +1,58 @@
 package main
 
-//import "fmt"
+import "fmt"
 
 var currentId int
-//var todos Todos
+var todos Todos
 var users Users
 
 func init() {
-	//RepoCreateTodo(Todo{Name: "Write presentation"})
-	//RepoCreateTodo(Todo{Name: "Host meetup"})
-	//RepoFindTodo(3)
+	RepoCreateTodo(Todo{Name: "Write presentation"})
+	RepoCreateTodo(Todo{Name: "Host meetup"})
 	CreateNewUser(User{Name: "userlol"})
 	CreateNewUser(User{Name: "user"})
 	CreateNewUser(User{Name: "test", Login: "testlogin", Password: "testpassword", Email: "test@mail.com"})
 }
 
-//func RepoFindTodo(id int) Todo{
-//	for _, t := range todos {
-//		if t.Id == id {
-//			return t
-//		}
-//	}
-//	//return empty todos if not created
-//	return Todo{}
-//}
-//
-//func RepoCreateTodo(t Todo) Todo{
-//	currentId += 1
-//	t.Id = currentId
-//	todos = append(todos, t)
-//	return t
-//}
-//
+func RepoFindTodo(id int) Todo{
+	for _, t := range todos {
+		if t.Id == id {
+			return t
+		}
+	}
+	//return empty todos if not created
+	return Todo{}
+}
+
+func RepoCreateTodo(t Todo) Todo{
+	currentId += 1
+	t.Id = currentId
+	todos = append(todos, t)
+	return t
+}
+
 func CreateNewUser(t User) User{
 	currentId += 1
 	t.Id = currentId
 	users = append(users, t)
 	return t
 }
-//
-//func RepoDestroyTodo(id int) error{
-//	for i, t := range todos {
-//		if t.Id == id {
-//			todos = append(todos[:i], todos[i+1:]...)
-//			return nil
-//		}
-//	}
-//	return fmt.Errorf("Could not find Todo with id of %d to delete", id)
-//}
+
+func UserFind(id int) User{
+	for _, t := range users {
+		if t.Id == id {
+			return t
+		}
+	}
+	return User{}
+}
+
+func RepoDestroyTodo(id int) error{
+	for i, t := range todos {
+		if t.Id == id {
+			todos = append(todos[:i], todos[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("Could not find Todo with id of %d to delete", id)
+}
